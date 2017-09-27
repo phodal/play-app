2a381966b5151b14706c66f3b1679ee2702d95b5
Fengda HUANG <h@phodal.com>
2017-09-27 21:19:24 +0800

    [T] add react-native-device-info

--- android/app/src/main/java/com/play/MainApplication.java
+++ android/app/src/main/java/com/play/MainApplication.java
@@ -3,6 +3,7 @@ package com.play;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNDeviceInfo(),
             new FastImageViewPackage(),
             new RNSpinkitPackage(),
         new VectorIconsPackage()
