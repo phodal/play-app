bc7bdd70ab5f0ab5e8010f6681f1bbf9657fbb0e
Phodal HUANG <h@phodal.com>
2017-10-09 08:05:43 +0800

    [T] clean code

--- src/utils/MarkdownHelper.js
+++ src/utils/MarkdownHelper.js
@@ -1,21 +1,3 @@
-// const hljs = require('highlightjs'); // https://highlightjs.org/
-//
-// const md = require('markdown-it')({
-//   highlight(str, lang) {
-//     if (lang && hljs.getLanguage(lang)) {
-//       try {
-//         return `<pre class="hljs"><code>${
-//           hljs.highlight(lang, str, true).value
-//           }</code></pre>`.replace(/\n/g, '<br>');
-//       } catch (__) {
-//         console.error(__);
-//       }
-//     }
-//
-//     return `<pre class="hljs"><code>${md.utils.escapeHtml(str).replace(/\n/g, '<br>')}</code></pre>`;
-//   }
-// });
-
 const md = require('markdown-it')();
 const isString = require('lodash.isstring');
 
