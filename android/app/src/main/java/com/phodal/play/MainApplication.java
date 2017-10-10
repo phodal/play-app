3ec1cfc17548de58f13d262689e284516026a3aa
Fengda HUANG <h@phodal.com>
2017-10-10 23:11:49 +0800

    [T] remove unused files

--- android/app/src/main/java/com/phodal/play/MainApplication.java
+++ android/app/src/main/java/com/phodal/play/MainApplication.java
@@ -45,6 +45,11 @@ public class MainApplication extends Application implements ReactApplication {
         new GoogleAnalyticsBridgePackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
