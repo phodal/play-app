736e5ea197211550d4882a8e5fe22e1b1dcd5799
Phodal HUANG <h@phodal.com>
2017-10-23 07:50:27 +0800

    [LOGIN] add basic view

--- src/modules/login/LoginView.js
+++ src/modules/login/LoginView.js
@@ -1,10 +1,17 @@
 /* eslint-disable max-nested-callbacks */
 import React, {Component} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import TimerMixin from 'react-timer-mixin';
-import reactMixin from 'react-mixin';
+import {Dimensions, StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+
+import * as Tcomb from 'tcomb-form-native';
+const Form = Tcomb.form.Form;
 
 const window = Dimensions.get('window');
+
+const Person = Tcomb.struct({
+  name: Tcomb.String,
+  password: Tcomb.String
+});
+const options = {}; // optional rendering options (see documentation)
 
 export default class App extends Component {
   constructor(props) {
@@ -14,13 +21,19 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Hello, world</Text>
+        <View style={styles.formContainer}>
+          <Form
+            type={Person}
+            options={options}
+          />
+          <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>登录</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
-
-reactMixin(App.prototype, TimerMixin);
 
 const styles = StyleSheet.create({
   container: {
@@ -28,5 +41,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     width: window.width,
     height: window.height
+  },
+  formContainer: {
+    margin: 20
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
