963f41c32c8f86bdd95b1f61f315d5c7674cd929
Fengda HUANG <h@phodal.com>
2017-10-28 15:09:42 +0800

    [APP Indexing] add request in deatail page

--- src/modules/play/PlayDetailView.js
+++ src/modules/play/PlayDetailView.js
@@ -8,11 +8,13 @@ import {
   StyleSheet
 } from 'react-native';
 import FastImage from 'react-native-fast-image';
+import axios from 'axios';
 
 import {URL} from '../../constants';
 import MarkdownHelper from '../../utils/MarkdownHelper';
 import NativeHtmlViewRender from '../../wdui/NativeHtmlViewRender';
 import Divider from '../../wdui/Divider';
+import Loading from '../../wdui/loading/Loading';
 import normalize from '../../wdui/helpers/normalizeText';
 
 const deviceWidth = Dimensions.get('window').width;
@@ -30,30 +32,51 @@ class PlayDetailView extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      data: props.navigation.state && props.navigation.state.params ? props.navigation.state.params : null
+    };
+  }
+
+  componentWillMount() {
+    console.log(this.props);
+    if (this.props.navigation.state.params && this.props.navigation.state.params.slug) {
+      let slug = this.props.navigation.state.params.slug;
+      axios.get(URL.PLAY + '?slug=' + slug)
+        .then(response => {
+          this.setState({
+            data: response.data.results[0],
+            loading: false
+          });
+        });
+    }
   }
 
   render() {
-    const {params} = this.props.navigation.state;
-    const htmlContent = MarkdownHelper.convert(params.content, {width: deviceWidth});
+    let data = this.state.data;
+    if (!data && !data.content) {
+      return <Loading text={'数据加载中'} />;
+    }
+    const htmlContent = MarkdownHelper.convert(data.content, {width: deviceWidth});
 
     return (
       <View style={styles.container}>
         <ScrollView>
           <View style={{paddingBottom: 15}}>
-            { params.featured_image
+            { data.featured_image
               ? <FastImage
                 style={{width: deviceWidth, height: (deviceWidth) * 0.4}}
                 resizeMode={FastImage.resizeMode.cover}
                 source={{
-                  uri: URL.IMAGE_BASE + params.featured_image
+                  uri: URL.IMAGE_BASE + data.featured_image
                 }}
               />
               : <View />
             }
           </View>
-          <Text style={styles.title}>{ params.title }</Text>
+          <Text style={styles.title}>{ data.title }</Text>
           <View style={styles.authorInfo}>
-            <Text style={styles.author}>{ params.user }</Text>
+            <Text style={styles.author}>{ data.user }</Text>
           </View>
           <Divider />
           <NativeHtmlViewRender
